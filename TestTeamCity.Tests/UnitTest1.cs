b278937dad276b43d3360023f977bdc88010aa9b
tkarpuk <t.karpuk@itransition.com>
2022-02-07 10:56:35 +0200

    Add project files.

---
+++ TestTeamCity.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace TestTeamCity.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        [TestCase(1, 1, 2)]
+        public void GetSum_ValidArguments_ResulCorrectSum(int a, int b, int expectedSum)
+        {
+            var actualSum = Program.GetSum(a, b);
+
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+    }
+}
---
+++ TestTeamCity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestTeamCity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetSum(10, 20));
+            Console.WriteLine("-----------------------------------\nThe END!");
+        }
+
+        public static int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
